d9458584b3666a5915d6350bd67f1a1a6c98e73e
StevesyZissou <otto.nagengast@gmail.com>
2020-08-26 12:53:33 -0400

    Added base list and item

---
+++ src/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Item from './Item.js';
+
+export default function List() {
+  return <div>
+    <Item label="Buy groceries" />
+    <Item label="Walk dog"/>
+  </div>
+}
